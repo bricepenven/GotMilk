5db0e89085b3b0101b7b232d72b05587054b09eb
Brice Penven <brice.penven@icloud.com>
2025-05-28 19:23:46 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -8,9 +8,10 @@ const firebaseConfig = {
     appId: "1:628147483032:web:2cea7a3dd553b8922d7398"
 };
 
-const rawWebhookUrl = "https://invigorating-happiness.up.railway.app//webhook/884e09b7-11b7-4728-b3f7-e909cc9c6b9a";
-const webhookUrl = rawWebhookUrl.replace(/([^:]\/)\/+/g, "$1"); // removes extra slashes except after https://
+// Webhook URL for n8n workflow
+const webhookUrl = "https://invigorating-happiness.up.railway.app/webhook-test/884e09b7-11b7-4728-b3f7-e909cc9c6b9a";
 
+// CORS proxy URL as a fallback
 const corsProxyUrl = "https://corsproxy.io/?";
 
 export { firebaseConfig, webhookUrl, corsProxyUrl };
